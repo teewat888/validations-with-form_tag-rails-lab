f02f8f838b85e1effe9e8f2218f8eedb3dfb6df2
TEERAWAT PRAROM <teewat@yahoo.com>
2022-01-21 21:31:10 +1100

    completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,24 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
+       redirect_to post_path(@post)
+    else
+       render :edit
+    end
+  end
+  def new
+    @post = Post.new
+  end
 
-    redirect_to post_path(@post)
+  def create
+    @post = Post.new(post_params)
+    if@post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
